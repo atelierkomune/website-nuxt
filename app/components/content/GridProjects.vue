58034b302d2a5fe1a7a49f259ee47c9a562f238a
Adrien Becker <adrien.becker@gmail.com>
2025-04-11 16:00:55 +0200

    style: add img to project grid

--- app/components/content/GridProjects.vue
+++ app/components/content/GridProjects.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
 const { data: projects } = await useAsyncData(
   'projects-content-grid', 
-  () => queryCollection('projects').select('title', 'path', 'image').all(),
+  () => queryCollection('projects').select('title', 'path', 'image', 'tags').all(),
 )
 </script>
 <template>
-  {{ projects }}
   <div v-for="project in projects" :key="project.path">
     <NuxtLink :to="project.path">
-      {{ project.title }}
-      <pre>
-        {{ project }}
-      </pre>
+      {{ project.image }}
+      <img :src="project.image" :alt="project.title" class="w-full h-full object-cover" />
+      <span>{{ project.title }}</span>
     </NuxtLink>
   </div>
 </template>
