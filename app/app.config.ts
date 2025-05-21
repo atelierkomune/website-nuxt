1f5db26c394973385ecf05df5e95f72e3aeda86a
atelierkomune <atelier.komune@gmail.com>
2025-05-21 16:39:19 +0200

    3 changes by Nuxt Studio

--- app/app.config.ts
+++ app/app.config.ts
@@ -6,11 +6,11 @@ export default defineAppConfig({
     darkMode: true,
     diapo: {
       images: {
-        one: '/projects/ATELIER-MATERIA_CENTRE-TECHNIQUE/ATELIER-MATERIA_CENTRE-TECHNIQUE-AJACCIO-02.jpg',
-        two: '/projects/ATELIER-MATERIA_CENTRE-TECHNIQUE/ATELIER-MATERIA_CENTRE-TECHNIQUE-AJACCIO.jpg',
-        three: '/projects/LANOIRE-N-COURRIAN/LANOIRE-N-COURRIAN_LOGEMENTS-BORDEAUX.jpg',
-        four: '/projects/BERRANGER-N-VINCENT/BERRANGER-N-VINCENT_MEDIATHEQUE-SAINT-YVI-02.jpg',
-        five: '/projects/ATELIER-MATERIA_LOGEMENTS-APPIETTO/ATELIER-MATERIA_LOGEMENTS-APPIETTO-02.jpg',
+        one: '/projects/LANDING/36-b-compressed.jpg',
+        two: '',
+        three: '',
+        four: '',
+        five: '',
       }
     },
     fonts: {
